40e52153ae87ce2f9e80860149d1c28fabecab67
Dalton Natale <cdalton.natale@gmail.com>
2021-03-09 18:25:38 -0700

    set up web layer, controller, get request

---
+++ bankdemo/src/main/kotlin/com/example/bankdemo/controller/BankController.kt
@@ -0,0 +1,15 @@
+package com.example.bankdemo.controller
+
+import com.example.bankdemo.model.Bank
+import com.example.bankdemo.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks()
+}
